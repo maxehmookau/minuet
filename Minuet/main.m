fe26346009a3c757309d56d9bf7a40853c843353
Max Woolf <maximus@zingysaturn.co.uk>
2012-11-21 20:38:38 +0000

    Initial Commit

---
+++ Minuet/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Minuet
+//
+//  Created by Max Woolf on 21/11/2012.
+//  Copyright (c) 2012 MaxWoolf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
